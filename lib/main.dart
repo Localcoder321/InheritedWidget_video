2651d887563643dab818479b536883d8e210f550
Localcoder <wiut00011625@gmail.com>
2023-09-04 14:13:19 +0500

    step 4

--- lib/main.dart
+++ lib/main.dart
@@ -51,9 +51,64 @@ class AppStateScope extends InheritedWidget {
   bool updateShouldNotify(AppStateScope oldWidget) {
    return data != oldWidget.data;
   }
-
-
 }
+
+class AppStateWidget extends StatefulWidget {
+  const AppStateWidget({required this.child, Key? key}) : super(key: key);
+
+  final Widget child;
+
+  static _AppStateWidgetState of(BuildContext context) {
+    return context.findAncestorStateOfType<_AppStateWidgetState>()!;
+  }
+
+  @override
+  State<AppStateWidget> createState() => _AppStateWidgetState();
+}
+
+class _AppStateWidgetState extends State<AppStateWidget> {
+  AppState _data = AppState(productList: Server.getProductList());
+
+  void setProductList(List<String> newProductList){
+    if(newProductList != _data.productList) {
+      setState(() {
+        _data = _data.copyWith(
+          productList: newProductList,
+        );
+      });
+    }
+  }
+
+  void addToCart(String id) {
+    if(!_data.itemsInCart.contains(id)){
+      final Set<String> newItemsInCart = Set<String>.from(_data.itemsInCart);
+      newItemsInCart.add(id);
+      setState(() {
+        _data = _data.copyWith(
+          itemsInCart: newItemsInCart,
+        );
+      });
+    }
+  }
+
+  void removeFromCart(String id) {
+    if(!_data.itemsInCart.contains(id)) {
+      final Set<String> newItemsInCart =Set<String>.from(_data.itemsInCart);
+      newItemsInCart.remove(id);
+      setState(() {
+        _data = _data.copyWith(
+          itemsInCart: newItemsInCart,
+        );
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppStateScope(_data, child: widget.child);
+  }
+}
+
 
 class MyStorePage extends StatefulWidget {
   const MyStorePage({Key? key}) : super(key: key);
